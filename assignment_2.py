a00e3b8c16ac130247c001621893388ec6ebbc91
bree parker <breannaparker1991@gmail.com>
2022-09-11 21:13:19 -0600

    user_name

---
+++ assignment_2.py
@@ -0,0 +1,16 @@
+#DSC 510-T304
+#Week 2
+#Programming Assignment 2.1
+#Author Breanna Parker
+#9/11/22
+
+# Display a welcome message for your user.
+# Retrieve the company name from the user.
+# Retrieve the number of feet of fiber optic cable to be installed from the user.
+# Calculate the installation cost of fiber optic cable by multiplying the total cost as the number of feet times $0.87.
+# Print a receipt for the user including the company name, number of feet of fiber to be installed, the calculated cost, and total cost in a legible format.
+# Include appropriate comments throughout the program.
+# Your program should adhere to PEP8 guidelines especially as it pertains to variable names.
+
+user_name = input('What is your name?\n')
+print("Hello", user_name)
