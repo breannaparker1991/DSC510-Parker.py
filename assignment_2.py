7fb00cc2a62a8c16d7fe42522faa4680510328d4
bree parker <breannaparker1991@gmail.com>
2022-09-11 21:51:11 -0600

    added in comments

--- assignment_2.py
+++ assignment_2.py
@@ -12,17 +12,22 @@
 # Include appropriate comments throughout the program.
 # Your program should adhere to PEP8 guidelines especially as it pertains to variable names.
 
+from unicodedata import name
+
+
 user_name = input('What is your name?\n')
 print("Hello",user_name)
 
-company_name = input("What is your company name?\n")
-num_feet_optic_cable = float(input("What is the number of feet of fiber optic cable to be installed?\n"))
-cost_per_foot = 0.87
-two_decimals = "{:.2f}".format(cost_per_foot)
-cost = num_feet_optic_cable * cost_per_foot
-cost = round(cost,2)
+company_name = input("What is your company name?\n") #user's company
+num_feet_optic_cable = float(input("What is the number of feet of fiber optic cable to be installed?\n")) #input from user number of feet needed
+cost_per_foot = 0.87 #designated cost per foot
+two_decimals = "{:.2f}".format(cost_per_foot) #making sure decimals are included in calculations
+cost = num_feet_optic_cable * cost_per_foot #calculating total cost for user
+cost = round(cost,2) #rounding cost to two decimal places
 
+#final printed recipe
+print("Recipt for", user_name)
 print("Company Name:",company_name)
-print("Number of Optic Feet:", num_feet_optic_cable) 
-print("Cost per foot:","", "$",two_decimals)
-print("Total Cost:", cost)
+print("Number of Optic Feet:", num_feet_optic_cable,"ft") 
+print("Cost per foot:", "$",two_decimals)
+print("Total Cost:","$", cost)
